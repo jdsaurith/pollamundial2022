f87de4b748eaf176bf1f7ed5a5c6bd283b92fb04
Jefferson Saurith <jeffersonsaurith@gmail.com>
2022-11-18 19:58:42 -0300

    jeffer
    subir

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: 'https://servidorrissetto.de:433/api/'
+    baseURL: 'https://servidorrissetto.de:443/api/'
     // baseURL: 'http://localhost:5000/api/'
     // baseURL: 'https://servidormercadocentral-production.up.railway.app/api/'
 });
